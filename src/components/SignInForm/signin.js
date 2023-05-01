7c81a288ab4c43fc6330005eb75b251fffc38b5a
Kyle Patrick Bader <kbader91@gmail.com>
2023-05-01 16:38:26 -0700

    signup and signin formik formats in place

--- src/components/SignInForm/signin.js
+++ src/components/SignInForm/signin.js
@@ -1,20 +1,65 @@
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from '../../firebase/index';
+import { useState } from 'react';
+import { Formik } from "formik";
 import './signin.css';
 
+// validation
+const validate = values => {
+    const errors = {};
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    } return errors;
+}
+
 const SignInForm = () => {
-    // const nav = useNavigate();
+    const [error, setError] = useState(null);
+    const nav = useNavigate();
+    const initialValues = {
+        email: '',
+        password: ''
+    };
+
+    const handleSubmit = async (values, { setSubmitting }) => {
+        const { email, password } = values;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            setSubmitting(false);
+            nav("/gallery");
+        } catch (error) {
+            console.log(error);
+            setSubmitting(false);
+            setError(error)
+        }
+    };
+
     return (
-        <form id="sign-in-form">
-            <h1 id="sign-in-header">Sign in</h1>
-            <input className="si-form-slot" placeholder="Email" />
-            <input className="si-form-slot"  type="password" placeholder="Password" />
-            <button id="sign-in-btn">Sign in</button>
-            <div id="sign-up-link">
-                <p>Don't have an account?</p>
-                <Link to="/sign-up">Sign up</Link>
-            </div>
-        </form>
+        <Formik initialValues={initialValues} validate={validate} onSubmit={handleSubmit}>
+            {
+                ({ values, errors, handleChange, handleSubmit, isSubmitting }) => {
+                    const { email } = errors;
+                    return (
+                        <form id="sign-in-form" onSubmit={handleSubmit}>
+                            <h1 id="sign-in-header">Sign in</h1>
+                            <input className={"si-form-slot" + (email ? 'error' : '')} type="email" placeholder="Email" name="email" onChange={handleChange} value={values.email} />
+                            <input className="si-form-slot" type="password" placeholder="Password" name="password" onChange={handleChange} value={values.password} />
+                            <button id="sign-in-btn" type="submit" disabled={isSubmitting}>Sign in</button>
+                            <div id="sign-up-link">
+                                <p>Don't have an account?</p>
+                                <Link to="/sign-up">Sign up</Link>
+                            </div>
+                            <div className="error-message">
+                                {
+                                    error && <p>{error.message}</p>
+                                }
+                            </div>
+                        </form>
+                    );
+                }
+            }
+        </Formik>
     );
 };
 
--- src/components/SignUp/signup.js
+++ src/components/SignUp/signup.js
@@ -4,19 +4,30 @@ import Fade from 'react-reveal/Fade';
 import { firestore, createUserProfileDocument, auth } from '../../firebase/index';
 import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { validateEmail } from '../../utils/helpers';
+import { Formik } from 'formik';
+
+// validation
+const validate = values => {
+    const errors = {};
+    if (!values.email) {
+        errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = "Invalid email address";
+    }
+    if (!values.userName) {
+        errors.userName = "Required";
+    }
+    if (!values.password) {
+        errors.password = "Required";
+    }
+
+    return errors;
+};
 
 const SignUp = () => {
 
-    // const form = useRef();
-    // const [signUpForm, setSignUpForm] = useState({ firstName: '', lastName: '', location: '', email: '', password: '' });
-    // const { firstName, lastName, location, email, password } = signUpForm; 
-    // const [errorMessage, setErrorMessage] = useState('');
-
     const initialValues = {
-        firstname: '',
-        lastname:'',
-        location:'',
+        userName: '',
         email: '',
         password: ''
     }
@@ -24,12 +35,12 @@ const SignUp = () => {
     const [error, setError] = useState(null);
 
     const handleSignUp = async (values, { setSubmitting }) => {
-        const { firstname, lastname, location, email, password } = values;
+        const { userName, email, password } = values;
 
         try {
             const { user } = await auth.createUserWithEmailAndPassword(email, password);
             // call createUserProfileDocument function 
-            await createUserProfileDocument(user, values);
+            await createUserProfileDocument(user, { displayName: userName });
             nav('/gallery');
             setSubmitting(false);
         } catch (err) {
@@ -44,18 +55,31 @@ const SignUp = () => {
     return (
         <section id="sign-up-page">
             <Fade up>
-                <form id="sign-up-form">
-                    <h1 id="sign-up-header">Sign Up</h1>
-                    <input className="su-form-slot" placeholder="First Name" />
-                    <input className="su-form-slot" placeholder="Last Name" />
-                    <input className="su-form-slot" placeholder="Location" />
-                    <input className="su-form-slot" placeholder="Email" />
-                    <input className="su-form-slot" placeholder="Password" />
-                    <div id="su-btn-cancel-opt">
-                        <button id="sign-up-btn">Sign Up</button>
-                        <Link to="/">Cancel</Link>
-                    </div>
-                </form>
+                <Formik initialValues={initialValues} validate={validate} onSubmit={handleSignUp}>
+                    {
+                        ({ values, errors, handleChange, handleSubmit, isSubmitting }) => {
+                            const { userName, email, password } = errors;
+                            return (
+                                <form id="sign-up-form" onSubmit={handleSubmit}>
+                                    <h1 id="sign-up-header">Sign Up</h1>
+                                    <input className={ "su-form-slot" + (userName ? "error" : "")} placeholder="User Name" type="text" name="userName" onChange={handleChange} value={values.userName} />
+                                    <input className={ "su-form-slot" + (email ? "error" : "")} placeholder="Email" type="email" name="email" onChange={handleChange} value={values.email} />
+                                    <input className={ "su-form-slot" + (password ? "error" : "")} placeholder="Password" type="password" name="password" onChange={handleChange} value={values.password} />
+
+                                    <div id="su-btn-cancel-opt">
+                                        <button id="sign-up-btn" type="submit" disabled={isSubmitting}>Sign Up</button>
+                                        <div className="error-message">
+                                            {
+                                                error && <p>{error.message}</p>
+                                            }
+                                        </div>
+                                        <Link to="/">Cancel</Link>
+                                    </div>
+                                </form>
+                            );
+                        }
+                    }
+                </Formik>
             </Fade>
         </section>
     )
